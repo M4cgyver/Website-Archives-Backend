69346f9b3bdb8972e4b581c9b2752a80f35ceb24
M4cgyver <m4cgyverisvoid@gmail.com>
2024-07-23 03:04:42 -0700

    fixed bug

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
+import { resolve } from "bun";
 import { hexToBn } from "./libs/bignumber";
 import { connectDb, setupDb } from "./libs/database";
 import { listenExpress, setupExpress } from "./libs/express";
 import { parseWarcFiles } from "./libs/warcs";
+
+await new Promise(resolve=>setTimeout(resolve, 5000))
 
 console.log("starting api");
 
